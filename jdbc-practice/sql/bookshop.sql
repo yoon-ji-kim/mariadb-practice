7471828e5b6bcd5aa30365ad0259d0f45b52d924
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-01-12 17:41:58 +0900

    bookshol sql query

--- jdbc-practice/sql/bookshop.sql
+++ jdbc-practice/sql/bookshop.sql
@@ -7,4 +7,20 @@ select no, name
 from author
 order by no;
 
-insert into author values(null, ?);
+insert into author values(null, "김윤지");
+delete from author
+where name ='김윤지';
+insert into book(no, title, author_no) values(null, "트와일라잇", 1);
+select * from book;
+
+select a.no, a.title, a.rent, b.name
+from book a
+join author b
+on a.author_no = b.no;
+
+update book
+set rent = 'Y'
+where no = ;
+select no, rent
+from book
+where no = 1;
